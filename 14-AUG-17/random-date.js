c3cfe6b9eb05c2b105b126258a7d32d4ec315800
prakash <mailmeprakash1992@gmail.com>
2017-08-17 20:25:39 +0530

    Corrected Random code file

--- 14-AUG-17/random-date.js
+++ 14-AUG-17/random-date.js
@@ -12,11 +12,23 @@ for (var d = 1; d <= 25; d++) {
     for (var m = 1; m <= 12; m++) {
     	var date = m + i + d + i + y;
     	dateStrings.push(date)
+    	console.log (Date)
     }
 }
 
 dateStrings = dateStrings.sort(function(a, b) {
-	return (new Date(a) - new Date(b))
-})
+	return (new Date(a) - new Date(b));
+});
 
-console.log(dateStrings)
+
+dateStrings = dateStrings.map(date => (new Date(date)).toDateString());
+
+
+
+
+ function pickRandomDate(dates) {
+	return Math.floor(Math.random()*dateStrings.length);
+
+}
+ dateStrings = dateStrings[pickRandomDate()];
+console.log(dateStrings);
