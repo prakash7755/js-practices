6b6b4a70db176f5557638e249454ad07262d68de
prakash <mailmeprakash1992@gmail.com>
2017-09-08 00:54:29 +0530

    today practices

--- 05-sep-2017/server.js
+++ 05-sep-2017/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var multer  = require('multer');
-var upload = multer({ dest: '../uploads/' });
+var upload = multer({ dest: 'uploads/' });
 var router = express.Router();
 var path = require('path');
 
@@ -11,8 +11,13 @@ app.get('/', function (req, res) {
 	res.sendFile(path.resolve('./file upload.html'));
 });
 
-app.post('/uploadfile', upload.single('image'), function (req, res, next) {
-console.log(req.file)
+app.post('/uploadfile', upload.single('file'), function (req, res, next) {
+    console.log(req.file)
+    if (req.file) {
+     return res.json({isSuccess: true, file: req.file});
+    } else {
+    	res.json({isSuccess: false, message: 'Sorry'})
+    }
 });
 
 
